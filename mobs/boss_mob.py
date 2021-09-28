21a7578b6d1f2096d0f34cda69928ac2ba9fb5d0
hugokbr <91246180+hugokbr@users.noreply.github.com>
2021-09-28 09:55:50 +0200

    Ajout mob boss

---
+++ mobs/boss_mob.py
@@ -0,0 +1,79 @@
+import pictures
+import mobs.mob
+from position import Position, TilePosition, Direction
+import main
+import game
+
+pictures.load_picture("carre", "mobs/")
+
+
+class BossMob(mobs.mob.Mob):
+    def __init__(self, game_, position: Position):
+        attributes = {
+            
+            # la vie maximum
+            "health": 100,
+            
+            # la vitesse, en tuiles/ticks
+            # TICK_REAL_TIME devrait toujours etre 0.1 (secondes) mais il faut toujours utiliser la variable
+            # mettre TICK_REAL_TIME comme speed revient a dire que le mob avancera d'une tuile par seconde
+            # ici TICK_REAL_TIME / 2 veut dire qu'il mettra 2 secondes a traverser une tuile
+            "speed": main.TICK_REAL_TIME / 2.5,
+            
+            # les resistances du mob, pour chaque type d'attaque
+            "resistances": {
+                # une résistance de 2 indique que le mob prendra 2x moins de dégats de feu
+                game.DAMAGE_TYPE_FIRE: 0.75,
+                
+                # une résistance de 0.5 indique que le mob prendra 2x plus de dégats de glace
+                game.DAMAGE_TYPE_ICE: 0.75,
+                
+                # résistance de 1: pas d'altération
+                game.DAMAGE_TYPE_RAW: 0.75,
+                game.DAMAGE_TYPE_MAGIC: 0.75,
+                
+                # le type DAMAGE_TYPE_ABSOLUTE est renseigné par défaut dans mob.py
+            },
+        }
+        super().__init__(game_, position, attributes)
+    
+    def tick(self):
+        # a chaque tick
+        
+        # le mob avance
+        self.advance()
+    
+    def get_render(self, time):
+        return pictures.PICTURES["carre"].get_img(time) "health": 6,
+            
+            # la vitesse, en tuiles/ticks
+            # TICK_REAL_TIME devrait toujours etre 0.1 (secondes) mais il faut toujours utiliser la variable
+            # mettre TICK_REAL_TIME comme speed revient a dire que le mob avancera d'une tuile par seconde
+            # ici TICK_REAL_TIME / 2 veut dire qu'il mettra 2 secondes a traverser une tuile
+            "speed": main.TICK_REAL_TIME / 2,
+            
+            # les resistances du mob, pour chaque type d'attaque
+            "resistances": {
+                # une résistance de 2 indique que le mob prendra 2x moins de dégats de feu
+                game.DAMAGE_TYPE_FIRE: 1,
+                
+                # une résistance de 0.5 indique que le mob prendra 2x plus de dégats de glace
+                game.DAMAGE_TYPE_ICE: 1,
+                
+                # résistance de 1: pas d'altération
+                game.DAMAGE_TYPE_RAW: 1,
+                game.DAMAGE_TYPE_MAGIC: 1,
+                
+                # le type DAMAGE_TYPE_ABSOLUTE est renseigné par défaut dans mob.py
+            },
+        }
+        super().__init__(game_, position, attributes)
+    
+    def tick(self):
+        # a chaque tick
+        
+        # le mob avance
+        self.advance()
+    
+    def get_render(self, time):
+        return pictures.PICTURES["carre"].get_img(time)
