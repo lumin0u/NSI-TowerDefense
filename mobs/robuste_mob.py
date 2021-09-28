695e70e49a449d3f660a2de4a0fd419d704eb83a
hugokbr <91246180+hugokbr@users.noreply.github.com>
2021-09-28 09:40:13 +0200

    ajout de deux mobs

--- mobs/robuste_mob.py
+++ mobs/robuste_mob.py
@@ -5,33 +5,33 @@ from position import Position, TilePosition, Direction
 import main
 import game
 
-pictures.load_picture("carre", "mobs/")
+pictures.load_picture("robuste", "mobs/")
 
 
-class SimpleMob(mobs.mob.Mob):
+class RobusteMob(mobs.mob.Mob):
     def __init__(self, game_, position: Position):
         attributes = {
             
             # la vie maximum
-            "health": 6,
+            "health": 10,
             
             # la vitesse, en tuiles/ticks
             # TICK_REAL_TIME devrait toujours etre 0.1 (secondes) mais il faut toujours utiliser la variable
             # mettre TICK_REAL_TIME comme speed revient a dire que le mob avancera d'une tuile par seconde
             # ici TICK_REAL_TIME / 2 veut dire qu'il mettra 2 secondes a traverser une tuile
-            "speed": main.TICK_REAL_TIME / 2,
+            "speed": main.TICK_REAL_TIME / 3,
             
             # les resistances du mob, pour chaque type d'attaque
             "resistances": {
                 # une résistance de 2 indique que le mob prendra 2x moins de dégats de feu
-                game.DAMAGE_TYPE_FIRE: 1,
+                game.DAMAGE_TYPE_FIRE: 0.5,
                 
                 # une résistance de 0.5 indique que le mob prendra 2x plus de dégats de glace
-                game.DAMAGE_TYPE_ICE: 1,
+                game.DAMAGE_TYPE_ICE: 0.5,
                 
                 # résistance de 1: pas d'altération
-                game.DAMAGE_TYPE_RAW: 1,
-                game.DAMAGE_TYPE_MAGIC: 1,
+                game.DAMAGE_TYPE_RAW: 0.5,
+                game.DAMAGE_TYPE_MAGIC: 1.5,
                 
                 # le type DAMAGE_TYPE_ABSOLUTE est renseigné par défaut dans mob.py
             },
@@ -45,4 +45,4 @@ class SimpleMob(mobs.mob.Mob):
         self.advance()
     
     def get_render(self, time):
-        return pictures.PICTURES["carre"].get_img(time)
+        return pictures.PICTURES["robuste"].get_img(time)
